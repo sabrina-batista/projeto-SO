a7ab3de9e849e0b2a1983e09836db8f84da0df5b
anabeatrizcavalcanti <ana.santos@ccc.ufcg.edu.br>
2024-05-04 21:01:49 -0300

    Ajustes nos cenários

--- simulador.py
+++ simulador.py
@@ -22,19 +22,19 @@ class Disco:
     
     def alocar_arquivo(self, tamanho_arquivo):
          #Aloca um arquivo na FAT usando o método First Fit
-        bloco_inicial = self.encontrar_bloco_livre() #2
+        bloco_inicial = self.encontrar_bloco_livre()
         if bloco_inicial != -1:
-            tamanho_restante = tamanho_arquivo #513
-            bloco_atual = bloco_inicial #2
+            tamanho_restante = tamanho_arquivo
+            bloco_atual = bloco_inicial
             blocos_alocados = []
             while tamanho_restante > 0 and bloco_atual < len(self.fat):
                 if self.fat[bloco_atual]["livre"]:
-                    tamanho_bloco_disponivel = self.tamanho_bloco #512
-                    tamanho_a_alocar = min(tamanho_restante, tamanho_bloco_disponivel) #1
+                    tamanho_bloco_disponivel = self.tamanho_bloco
+                    tamanho_a_alocar = min(tamanho_restante, tamanho_bloco_disponivel)
                     self.fat[bloco_atual]["livre"] = False
-                    tamanho_restante -= tamanho_a_alocar #0
+                    tamanho_restante -= tamanho_a_alocar
                     blocos_alocados.append(bloco_atual)
-                bloco_atual += 1 #4
+                bloco_atual += 1
             
             if len(blocos_alocados) > 1:
                 for i in range(len(blocos_alocados) - 1):
@@ -48,18 +48,16 @@ class Disco:
             # marca os blocos anteriores como livres novamente
                 for i in range(bloco_inicial, bloco_atual):
                     self.fat[i]["livre"] = True
-                return -2
+                return print("Não há espaço suficiente na FAT para alocar o arquivo. Todos os blocos são livres novamente.")
         else:
-            return -3 # Não há espaço suficiente na FAT para alocar o arquivo
+            return print("Não há espaço suficiente na FAT para alocar o arquivo")
 
     def ler_bloco(self, bloco):
-        # Lógica para simular leitura de um bloco
         # Calcular a latência de acesso usando os tempos de seek, rotação e transferência
         latencia = self.tempo_seek + self.tempo_rotacao + self.tempo_transferencia
         return latencia
 
     def escrever_bloco(self, bloco, dados):
-        # Lógica para simular escrita em um bloco
         # Calcular a latência de acesso usando os tempos de seek, rotação e transferência
         latencia = self.tempo_seek + self.tempo_rotacao + self.tempo_transferencia
         return latencia
@@ -76,41 +74,52 @@ def main():
     # Criar instância do disco
     disco = Disco(tamanho_bloco, trilhas, blocos_por_trilha, tempo_seek, tempo_rotacao, tempo_transferencia)
 
-    print("Estado Inicial da FAT")
+    print("===== Estado Inicial da FAT =====")
     for indice, bloco in enumerate(disco.fat):
             print(f'Bloco {indice}: {bloco}')
 
-    print("Cenário 1")
-    print("Alocando arquivo exatamente do tamanho de um bloco.")
+    print("----- Cenário 1 -----")
+    print("Alocando arquivo exatamente do tamanho de um bloco.",
+          "\nTamanho do bloco:", tamanho_bloco, "Bytes",
+          "\nTamanho do arquivo:", 512, "Bytes")
     bloco_alocado0 = disco.alocar_arquivo(512)
-    print("Bloco:", bloco_alocado0) # Bloco 0 alocado
+    print("Bloco", bloco_alocado0, "alocado.") # Bloco 0 alocado
 
-    print("Cenário 2")
-    print("Alocando 2 arquivos exatamente do tamanho de um bloco.")
+    print("----- Cenário 2 -----")
+    print("Alocando 2 arquivos exatamente do tamanho de um bloco.",
+          "\nTamanho do bloco:", tamanho_bloco, "Bytes",
+          "\nTamanho do arquivo 1:", 512, "Bytes",
+          "\nTamanho do arquivo 2:", 512, "Bytes")
     bloco_alocado1 = disco.alocar_arquivo(512)
     bloco_alocado2 = disco.alocar_arquivo(512)
-    print("Bloco:", bloco_alocado1) # Bloco 1 alocado
-    print("Bloco:", bloco_alocado2) # Bloco 2 alocado
+    print("Bloco", bloco_alocado1, "alocado") # Bloco 1 alocado
+    print("Bloco", bloco_alocado2, "alocado") # Bloco 2 alocado
 
-    print("Cenário 3")
-    print("Alocando arquivo menor do que o tamanho de um bloco.")
+    print("----- Cenário 3 -----")
+    print("Alocando arquivo menor do que o tamanho de um bloco.",
+          "\nTamanho do bloco:", tamanho_bloco, "Bytes",
+          "\nTamanho do arquivo:", 100, "Bytes")
     bloco_alocado3 = disco.alocar_arquivo(100)
-    print("Bloco:", bloco_alocado3)
+    print("Bloco", bloco_alocado3, "alocado") # Bloco 3 alocado
     desperdicio = tamanho_bloco - 100
-    print("Desperdício:", desperdicio, "Bytes")
+    print("Desperdício de", desperdicio, "Bytes")
 
-    print("Cenário 4")
-    print("Alocando arquivo maior do que o tamanho de um bloco.")
+    print("----- Cenário 4 -----")
+    print("Alocando arquivo maior do que o tamanho de um bloco.",
+          "\nTamanho do bloco:", tamanho_bloco, "Bytes",
+          "\nTamanho do arquivo:", 513, "Bytes")
     blocos_alocados = disco.alocar_arquivo(513)
-    print("Blocos:", blocos_alocados) # Blocos 4 e 5 serão alocados
-    print("Desperdício:", 513 - tamanho_bloco, "Bytes")
+    print("Blocos", blocos_alocados, "alocados") # Blocos 4 e 5 serão alocados
+    print("Desperdício de", 513 - tamanho_bloco, "Bytes")
 
-    print("Cenário 4.1")
-    print("Alocando arquivo maior do que o tamanho de um bloco, nesse caso, o ocupará todos os blocos livres restantes")
+    print("----- Cenário 4.1 -----")
+    print("Alocando arquivo maior do que o tamanho de um bloco, nesse caso, o ocupará todos os blocos livres restantes",
+          "\nTamanho do bloco:", tamanho_bloco, "Bytes",
+          "\nTamanho do arquivo:", 512 * 44, "Bytes")
     blocos_alocados1 = disco.alocar_arquivo(512 * 44)
-    print("Blocos:", blocos_alocados1)
+    print("Blocos", blocos_alocados1, "alocados")
 
-    print("Estado Final da FAT")
+    print("===== Estado Final da FAT =====")
     for indice, bloco in enumerate(disco.fat):
             print(f'Bloco {indice}: {bloco}')
 
